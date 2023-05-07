54c4dc35ba7033b24feefd24f524d693c9feed40
andi-nieves <andinieves151720@gmail.com>
2023-05-07 09:10:56 +0800

    Associate Elastic IP

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 
 import { ApolloProvider, ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
-const SERVER_ADDED = '192.168.254.115:4000';
+const SERVER_ADDED = '13.210.177.188:4000';
 
 const httpLink = new HttpLink({
   uri: `http://${SERVER_ADDED}/graphql`
